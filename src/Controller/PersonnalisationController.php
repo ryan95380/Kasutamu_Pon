8b55e417ebc21950a7a4ac163685d919eaec7abf
ryan95380 <ryanmambou270@gmail.com>
2026-05-19 12:10:05 +0200

    mise en place de la page de personnalisation

--- src/Controller/PersonnalisationController.php
+++ src/Controller/PersonnalisationController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\FigurineRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class PersonnalisationController extends AbstractController
+class PersonnalisationController extends AbstractController
 {
-    #[Route('/personnalisation', name: 'app_personnalisation')]
-    public function index(): Response
+    #[Route('/personnalisation/{id}', name: 'personnalisation')]
+    public function index(
+        int $id,
+        FigurineRepository $figurineRepository
+    ): Response
     {
-        return $this->render('personnalisation/index.html.twig', [
-            'controller_name' => 'PersonnalisationController',
-        ]);
+        $figurine = $figurineRepository->find($id);
+
+        if (!$figurine) {
+
+            throw $this->createNotFoundException(
+                'Figurine introuvable'
+            );
+
+        }
+
+        return $this->render(
+            'personnalisation/index.html.twig',
+            [
+                'figurine' => $figurine
+            ]
+        );
     }
 }
